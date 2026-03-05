ddace126fd80bf0e81aaf0d153c04b19045145f6
vmorenom27-glitch <vmorenom27@gmail.com>
2026-03-05 13:32:40 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -19,7 +19,7 @@ st.title("TRADUCTOR.")
 st.subheader("Escucho lo que quieres traducir.")
 
 
-image = Image.open('OIG7.jpg')
+image = Image.open('traductora.jpg.png')
 
 st.image(image,width=300)
 with st.sidebar:
